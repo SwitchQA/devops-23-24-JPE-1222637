5cb7cbb97778e72f94e11bf21330861bb9042622
ISEP_Switch_QA_Luis <126819903+SwitchQA@users.noreply.github.com>
2024-04-07 13:24:27 +0100

    test: adds a unit test for App class
    #10
    adds lib/ to gitignore

---
+++ ca2/part1/gradle_basic_demo/src/test/java/basic_demo/AppTest.java
@@ -0,0 +1,14 @@
+package java.basic_demo;
+
+import basic_demo.App;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+  @Test
+  public void testAppHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+}
